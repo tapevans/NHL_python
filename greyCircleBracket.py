6de78992591ccd64451d6bf300461b3c26f010e0
Tyler <47286889+tapevans@users.noreply.github.com>
2022-11-16 09:17:15 -0700

    CoreyShowedMeSomeThings

--- greyCircleBracket.py
+++ greyCircleBracket.py
@@ -1,37 +1,44 @@
+# %% 
 from PIL import Image
 import numpy as np
-Bracket = Image.open("TempBrackets\BracketBase.png")
-#display(Bracket)
 
-# XY positions of top left corner of the image
-# 
-Round0_vec = np.array(
-    [[200,  200],
-     [200,  900],
-     [200,  1500],
-     [200,  2200],
-     [200,  3000],
-     [200,  3700],
-     [200,  4300],
-     [200,  5000],
-     [5000, 200],
-     [5000, 900],
-     [5000, 1500],
-     [5000, 2200],
-     [5000, 3000],
-     [5000, 3700],
-     [5000, 4300],
-     [5000, 5000]])
+def make_grey_bracket():
+    Bracket = Image.open("TempBrackets\BracketBase.png")
+    #display(Bracket)
 
-r,c = Round0_vec.shape
+    # XY positions of top left corner of the image
+    # 
+    Round0_vec = np.array(
+        [[200,  200],
+        [200,  900],
+        [200,  1500],
+        [200,  2200],
+        [200,  3000],
+        [200,  3700],
+        [200,  4300],
+        [200,  5000],
+        [5000, 200],
+        [5000, 900],
+        [5000, 1500],
+        [5000, 2200],
+        [5000, 3000],
+        [5000, 3700],
+        [5000, 4300],
+        [5000, 5000]])
 
-Grey = Image.open("BracketLogos\Grey.png")
+    r,c = Round0_vec.shape
 
-# Loop through all rows to get all grey circles
-for i in range(r):
-    Bracket.paste(  Grey, tuple(Round0_vec[i]) )
-    
+    Grey = Image.open("BracketLogos\Grey.png")
 
-display(Bracket)
+    # Loop through all rows to get all grey circles
+    for i in range(r):
+        Bracket.paste(  Grey, tuple(Round0_vec[i]) )
+        
 
-Bracket.save("BracketResults\GreyBase.png")
+    display(Bracket)
+
+    Bracket.save("BracketResults\GreyBase.png")
+
+if __name__ == "__main__":
+    make_grey_bracket()
+# %%
